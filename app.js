2b9c766247cf994464eba9e85a862d1a31d1b2f7
Asad-Chalakha-DevOps <asad.c@peerbits.com>
2025-05-22 11:33:30 +0530

    testing for the build

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello from AWS ECS CI/CD!'));
+app.get('/', (req, res) => res.send('Hello from AWS ECS CI/CD! this is updated'));
 
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
